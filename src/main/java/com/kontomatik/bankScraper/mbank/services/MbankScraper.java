531f8a929349d2466214aba18c62bdbab3542bee
Maciej Franikowski <mfranikowski@gmail.com>
2024-06-29 17:13:08 +0200

    Add additional error handling and update properties.

--- src/main/java/com/kontomatik/bankScraper/mbank/services/MbankScraper.java
+++ src/main/java/com/kontomatik/bankScraper/mbank/services/MbankScraper.java
@@ -1,6 +1,7 @@
 package com.kontomatik.bankScraper.mbank.services;
 
 import com.kontomatik.bankScraper.cli.UserInteraction;
+import com.kontomatik.bankScraper.exceptions.ResponseHandlingException;
 import com.kontomatik.bankScraper.exceptions.ScrapingException;
 import com.kontomatik.bankScraper.mbank.models.AccountGroups;
 import com.kontomatik.bankScraper.mbank.models.RequestParams;
@@ -11,6 +12,8 @@ import org.jsoup.Connection;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 
 @Component
 public class MbankScraper {
@@ -20,6 +23,9 @@ public class MbankScraper {
 
     @Value("${mbank.accounts.url}")
     private String mbankScraperUrl;
+
+    @Value("${mbank.base.url}")
+    private String baseUrl;
 
     public MbankScraper(UserInteraction userInteraction, ResponseHandler responseHandler, JsoupClient jsoupClient) {
         this.userInteraction = userInteraction;
@@ -34,13 +40,20 @@ public class MbankScraper {
                     .ignoreContentType(true)
                     .build();
             Connection.Response response = jsoupClient.sendRequest(
-                    mbankScraperUrl,
+                    baseUrl + mbankScraperUrl,
                     Connection.Method.GET,
                     requestParams);
             AccountGroups groups = responseHandler.handleResponse(response.body(), AccountGroups.class);
+            validateScrapedGroups(groups);
             System.out.println(userInteraction.formatAccountGroups(groups));
-        } catch (Exception e) {
+        } catch (ScrapingException | IOException | ResponseHandlingException e) {
             throw new ScrapingException("An error has occurred during scraping: " + e.getMessage());
+        }
+    }
+
+    private void validateScrapedGroups(AccountGroups groups) {
+        if (groups == null) {
+            throw new ScrapingException("AccountGroups is null");
         }
     }
 }
